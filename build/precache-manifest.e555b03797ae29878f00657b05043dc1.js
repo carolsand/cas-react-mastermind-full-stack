596c099ecc24d53503685dd4f4c2e910fc3bc261
Carol Sanders <carolsand@gmail.com>
2019-10-02 10:03:23 -0700

    Adding nodemon server configuration in server.js

---
+++ build/precache-manifest.e555b03797ae29878f00657b05043dc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee7729af5dd0d530ad85107fcdeb7fb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ad71981034762556073",
+    "url": "/static/css/main.f179332b.chunk.css"
+  },
+  {
+    "revision": "d515ca0a2612c289b864",
+    "url": "/static/js/2.98170bdd.chunk.js"
+  },
+  {
+    "revision": "5ad71981034762556073",
+    "url": "/static/js/main.9afa98ac.chunk.js"
+  },
+  {
+    "revision": "d7935058df3c459704b6",
+    "url": "/static/js/runtime-main.b4784087.js"
+  }
+]);
